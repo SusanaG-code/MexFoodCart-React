4369c93ec4afd89a064eb56ec54c1a36047e2c49
Susana <su.gonzalez.vejo@gmail.com>
2021-02-08 12:19:01 +0100

    Add button styles

--- my-react-project/src/components/Header.js
+++ my-react-project/src/components/Header.js
@@ -1,15 +1,13 @@
 import "../stylesheets/Header.css";
-import hat from "../images/sombrero.png";
-import cactus from "../images/cactus.png";
+import skull from "../images/skull.png";
+
 import cart from "../images/shop.png";
 
 const Header = (props) => {
   return (
     <div className="header">
       <div className="menu">
-        <img src={hat} alt="cart" className="hat"></img>
-
-        <img src={cactus} alt="cart" className="skull"></img>
+        <img src={skull} alt="cart" className="hat"></img>
       </div>
       <h1 className="title">Mexican Food Cart</h1>
       <div>
